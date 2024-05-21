0f86a99f587cf78419e91dee290df1b526a98ff2
ESeokhwan <huan99@hanmail.net>
2024-05-21 14:33:34 +0900

    init: initialize spring env

---
+++ src/main/java/com/example/discordcloneserver/DiscordCloneServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.discordcloneserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordCloneServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordCloneServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/discordcloneserver/DiscordCloneServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discordcloneserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordCloneServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
