5815b363522f829064ceb108ba98e6f868f88475
pkw3322 <pkw3322@naver.com>
2024-05-23 17:57:05 +0900

    fix: fix stomp

--- src/main/java/com/example/discordcloneserver/web/controller/ChatController.java
+++ src/main/java/com/example/discordcloneserver/web/controller/ChatController.java
@@ -1,7 +1,9 @@
 package com.example.discordcloneserver.web.controller;
 
+import com.example.discordcloneserver.data.ChannelDataManager;
 import com.example.discordcloneserver.data.ChatDataManager;
 import com.example.discordcloneserver.domain.dto.Chat;
+import com.example.discordcloneserver.error.exception.NotFoundException;
 import com.example.discordcloneserver.web.requestbody.ChatRequestBody;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -12,13 +14,18 @@ import org.springframework.stereotype.Controller;
 @RequiredArgsConstructor
 @Controller
 public class ChatController {
-  ChatDataManager dataManager = ChatDataManager.getInstance();
+  ChatDataManager chatDataManager = ChatDataManager.getInstance();
+  ChannelDataManager channelDataManager = ChannelDataManager.getInstance();
 
-  @MessageMapping("/{roomId}")
+  @MessageMapping("/chat/{roomId}")
   @SendTo("/send/{roomId}")
   public Chat chat(@DestinationVariable Long roomId, ChatRequestBody message) {
+    if (!channelDataManager.existsChannel(roomId)) {
+      throw new NotFoundException();
+    }
+
     Chat chat = Chat.init(message.senderUniqueName(), message.content(), roomId);
-    dataManager.addChat(chat);
+    chatDataManager.addChat(chat);
     return chat;
   }
 }
