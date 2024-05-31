dab23341f60472a21ff1ac1b3df0ec8a382e92e4
pkw3322 <pkw3322@naver.com>
2024-05-31 21:46:07 +0900

    feat: add channel initializer

--- src/main/java/com/example/discordcloneserver/DiscordCloneServerApplication.java
+++ src/main/java/com/example/discordcloneserver/DiscordCloneServerApplication.java
@@ -6,11 +6,13 @@ import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class DiscordCloneServerApplication {
 
 	public static void main(String[] args) {
